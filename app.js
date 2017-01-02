ca76b53e10e969d990483abeebf5b14dfa59541f
anandprajapati1 <apraj2@sapient.com>
2017-01-02 15:54:30 +0530

    slider background color will also change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.controller('colorPickerController', ['$scope',function($scope) {
     $scope.colorMinRange= 0;
     $scope.colorMaxRange= 255;
     
-    $scope.R=120,$scope.G=120,$scope.B=120;
+    $scope.R=255,$scope.G=255,$scope.B=255;
     $scope.resultColor='rgb(0,0,0)';
 
     $scope.colorchange=function () {  
