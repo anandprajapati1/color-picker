0fc9a2e599b53311715e95efb861e7ccef953bf5
Anand Kumar Prajapati <anapraja@publicisgroupe.net>
2019-06-17 11:42:03 +0530

    Lint code update

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+/* eslint-disable no-undef */
 var app = angular.module('myApp', []);
 
-app.controller('colorPickerController', ['$scope',function($scope) {
-    $scope.colorMinRange= 0;
-    $scope.colorMaxRange= 255;
-    
-    $scope.R=255,$scope.G=255,$scope.B=255;
-    $scope.resultColor='rgb(0,0,0)';
+app.controller('colorPickerController', ['$scope', function ($scope) {
+    $scope.colorMinRange = 0;
+    $scope.colorMaxRange = 255;
 
-    $scope.colorchange=function () {  
-        $scope.resultColor='rgb('+$scope.R+','+$scope.G+','+$scope.B+')'
+    $scope.R = 255, $scope.G = 255, $scope.B = 255;
+    $scope.resultColor = 'rgb(0,0,0)';
+
+    $scope.colorchange = function () {
+        $scope.resultColor = 'rgb(' + $scope.R + ',' + $scope.G + ',' + $scope.B + ')'
     }
     $scope.colorchange();
 }]);
