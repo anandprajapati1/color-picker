8acbf7eb85bb86b6c3b0e1aad7b43da0fec7d98d
Anand Kumar Prajapati <anapraja@publicisgroupe.net>
2019-06-17 14:52:15 +0530

    Added coverage test code

---
+++ app.test.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-magic-numbers */
+const Cover = require('./sampleCoverage');
+
+test('test1', () => {
+    var cover = new Cover();
+    
+    expect(cover.b).toBe(12);
+    // expect(cover.getName()).toBe('Anand');
+})
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3)
+})
---
+++ sampleCoverage.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+const CoverageTestClass = function() {
+    this.a = 'Anand';
+    this.b = 12;
+
+    this.getName = function() {
+        return this.a;
+    }
+}
+
+module.exports = CoverageTestClass;
